cbc1ba35e211120c643995ecb4423a4334877a48
Bobby Speirs <bobby.speirs@gmail.com>
2017-04-21 16:54:24 -0400

    Added basic search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import API_KEY from './key';
+import SearchBar from './components/search_bar';
 
 // Create the component
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Add component to the DOM
