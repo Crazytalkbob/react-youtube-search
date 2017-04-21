f30f4d443aec4a09f213dedde2708eefd2f547e7
Bobby Speirs <bobby.speirs@gmail.com>
2017-04-21 16:29:38 -0400

    added component files, and youtube api key + dependencies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import API_KEY from './key';
+
+// Create the component
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Add component to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
